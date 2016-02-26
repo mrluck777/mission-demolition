fcd8b8cc9bcf54c5edc4100e59f16bd4ee8b7ec7
Kyle Herve <ak-24@live.com>
2016-02-26 11:46:53 -0500

    Small tweeks
    bouncy version

--- unity-mission-demolition-master/Assets/__Scripts/FollowCam.cs
+++ unity-mission-demolition-master/Assets/__Scripts/FollowCam.cs
@@ -2,13 +2,13 @@
 using System.Collections;
 
 public class FollowCam : MonoBehaviour {
-	static public FollowCam S; // a FollowCam Singleton
+	static public FollowCam S; 
 
-	// Fields set in the Unity Inspector pane
+
 	public float easing = 0.05f;
 	public Vector2 minXY;
 	public bool _______;
-	// Fields set in dynamically
+
 	public GameObject poi; // The point of interest
 	public float camZ; // The desired Z pos of the camera
 	
@@ -17,42 +17,42 @@ public class FollowCam : MonoBehaviour {
 		camZ = this.transform.position.z;
 	}
 	
-	// Use this for initialization
+
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
+
 	void FixedUpdate () {
 		Vector3 destination;
 		if (poi == null) {
 			destination = Vector3.zero;
 		} else {
-			// Get the position of the poi
+
 			destination = poi.transform.position;
-			// if poi is a Projectile, check to see if it's at rest
+
 			if (poi.tag == "Projectile") {
-				// If it is sleeping (that is, not moving)
+
 				if (poi.rigidbody.IsSleeping()) {
-					// return to default view
+
 					poi = null;
 					Destroy(poi);
 					MissionDemolition.SwitchView("Both");
-					// in the next update
+
 					return;
 				}
 			}
 		}
-		// Limit the X & Y to minimum values
+
 		destination.x = Mathf.Max(minXY.x, destination.x);
 		destination.y = Mathf.Max(minXY.y, destination.y);
-		// Interpolate from the current Camera position toward destination
+
 		destination = Vector3.Lerp(transform.position, destination, easing);
-		// Retain a destination.z of camZ
+
 		destination.z = camZ;
-		// Set the camera to the destination
+
 		this.transform.position = destination;
-		// Set the orthographicSize of the Camera to keep Ground in view
+
 		this.camera.orthographicSize = destination.y + 10;
 	}
 }
--- unity-mission-demolition-master/Assets/__Scripts/ProjectileLine.cs
+++ unity-mission-demolition-master/Assets/__Scripts/ProjectileLine.cs
@@ -1,57 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
-// Remember, the following line is needed to use Lists
+
 using System.Collections.Generic;
 
 public class ProjectileLine : MonoBehaviour {
-	static public ProjectileLine S; // Singleton
+	static public ProjectileLine S; 
 
-	// Fields set in the Unity Inspector pane
+
 	public float minDist = 0.1f;
 	public bool ____________;
-	// Fields set dynamically
+
 	public LineRenderer line;
 	private GameObject _poi;
 	public List<Vector3> points;
 	
 	void Awake() {
-		S = this; // Set the singleton
-		// Get a reference to the LineRendere
+		S = this; 
+
 		line = GetComponent<LineRenderer>();
-		// Disable the LineRenderer until it's needed
+
 		line.enabled = false;
-		// Initialize the points List
+
 		points = new List<Vector3>();
 	}
 
-	// Use this for initialization
+
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
+
 	void FixedUpdate () {
 		if (poi == null) {
-			// If there is no poi, search for one
+
 			if (FollowCam.S.poi != null) {
 				if (FollowCam.S.poi.tag == "Projectile") {
 					poi = FollowCam.S.poi;
 				} else {
-					return; // Return if we didn't find a poi
+					return; 
 				}
 			} else {
-				return; // Return if we didn't find a poi
+				return; 
 			}	
 		}
-		// If there is a poi, it's loc is added every FixedUpdate
+	
 		AddPoint();
 		if (poi.rigidbody.IsSleeping()) {
-			// Once the poi is sleeping, if is cleared
+
 			poi = null;
 		}
 	}
 	
-	// This is a property (that is, a method masquerading as a field)
+
 	public GameObject poi {
 		get {
 			return ( _poi );
@@ -59,7 +59,7 @@ public class ProjectileLine : MonoBehaviour {
 		set {
 			_poi = value;
 			if (_poi != null) {
-				// When _poi is set to something new, it resets everything
+
 				line.enabled = false;
 				points = new List<Vector3>();
 				AddPoint();
@@ -67,7 +67,7 @@ public class ProjectileLine : MonoBehaviour {
 		}
 	}
 	
-	// This cna be used to clear the line directly
+
 	public void Clear() {
 		_poi = null;
 		line.enabled = false;
@@ -75,27 +75,27 @@ public class ProjectileLine : MonoBehaviour {
 	}
 	
 	public void AddPoint() {
-		// This is called to add a point to the line
+
 		Vector3 pt = _poi.transform.position;
 		if (points.Count > 0 && (pt - lastPoint).magnitude < minDist) {
-			// If the point isn't far enough from the last point, it returns
+
 			return;
 		}
 		if (points.Count == 0) {
-			// If this is the launch point...
+
 			Vector3 launchPos = Slingshot.S.launchPoint.transform.position;
 			Vector3 launchPosDiff = pt - launchPos;
-			// ...it adds an extra but of line to aid aiming later
+
 			points.Add(pt + launchPosDiff);
 			points.Add(pt);
 			line.SetVertexCount(2);
-			// Sets the first two points
+
 			line.SetPosition(0, points[0]);
 			line.SetPosition(1, points[1]);
-			// Enables the LineRenderer
+
 			line.enabled = true;
 		} else {
-			// Normal behavior of adding a point
+
 			points.Add (pt);
 			line.SetVertexCount(points.Count);
 			line.SetPosition(points.Count - 1, lastPoint);
@@ -103,11 +103,11 @@ public class ProjectileLine : MonoBehaviour {
 		}
 	}
 	
-	// Returns the location of the most recently added point
+
 	public Vector3 lastPoint {
 		get {
 			if (points == null) {
-				// If there are no points, returns Vector3.zero
+
 				return (Vector3.zero);
 			}
 			return (points[points.Count - 1]);
